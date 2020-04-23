ccd914933814e05dbe39735d21fed95ba76ac391
Pravardhan <pravardhan.green1998@gmail.com>
2020-04-23 18:59:34 +0530

    added parser

--- ParsingAutomator.py
+++ ParsingAutomator.py
@@ -1,2 +1,11 @@
 import argparse
 from lxml import etree
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', action='store', required=True,
+                    help="Specifies the input file")
+parser.add_argument('-o', '--output', action='store', required=True,
+                    help='Specifies the output file')
+parser.add_argument('-c', '--class', action='store', required=True,
+                    help='Specifies the class file')
+args = parser.parse_args()
