9382867bb6c10727e42d329e1e70f73dc39de59d
Pravardhan <pravardhan.green1998@gmail.com>
2020-04-23 19:39:33 +0530

    made iterator

--- ParsingAutomator.py
+++ ParsingAutomator.py
@@ -6,6 +6,21 @@ parser.add_argument('-i', '--input', action='store', required=True,
                     help="Specifies the input file")
 parser.add_argument('-o', '--output', action='store', required=True,
                     help='Specifies the output file')
-parser.add_argument('-c', '--class', action='store', required=True,
+parser.add_argument('-b', '--bean', action='store', required=True,
                     help='Specifies the class file')
 args = parser.parse_args()
+
+tree = etree.parse(args.input)
+root = tree.getroot()
+output = open(args.output, 'w')
+bean = open(args.bean, 'w')
+
+queue = [root]
+
+while len(queue):
+    node = queue.pop(0)
+    for child in node:
+        if len(child) == 0:
+            output.write(child.tag + '\n')
+        else:
+            queue.append(child)
