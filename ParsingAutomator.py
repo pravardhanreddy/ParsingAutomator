bcc03dc4fb199e560c05ec37cd3863c68f19f61c
Pravardhan <pravardhan.green1998@gmail.com>
2020-04-24 08:23:20 +0530

    add formatting for leaves

--- ParsingAutomator.py
+++ ParsingAutomator.py
@@ -1,6 +1,7 @@
 import argparse
 from lxml import etree
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', action='store', required=True,
                     help="Specifies the input file")
@@ -8,6 +9,8 @@ parser.add_argument('-o', '--output', action='store', required=True,
                     help='Specifies the output file')
 parser.add_argument('-b', '--bean', action='store', required=True,
                     help='Specifies the class file')
+parser.add_argument('-p', '--prefix', action='store', required=True,
+                    help='Specifies the prefix')
 args = parser.parse_args()
 
 tree = etree.parse(args.input)
@@ -25,6 +28,19 @@ while len(queue):
             leaves.append((node.tag, child.tag))
         else:
             queue.append(child)
+
+count_child = [y for (x, y) in leaves]
+leaves = list(dict.fromkeys(leaves))
+
+for p, c in leaves:
+    if count_child.count(c) == 1:
+        output.write(args.prefix + '.set' + p.capitalize() + c.capitalize() +
+                     '(nullStrToSpc(AascXmlParser.getValue((Element) ' + p.lower() +
+                     'Node, "' + c + '")));\n')
+    else:
+        output.write(args.prefix + '.set' + c.capitalize() +
+                     '(nullStrToSpc(AascXmlParser.getValue((Element) ' + p.lower() +
+                     'Node, "' + c + '")));\n')
 
 output.close()
 bean.close()
